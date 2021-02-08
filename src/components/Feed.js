b8b2efc0ce106509a87ab6989c4729723faa15e5
Shannon Grantski <grantski@pm.me>
2021-02-08 15:03:03 -0800

    rename "tweet" to "tweetBody"
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,23 +4,23 @@ import Tweet from "./Tweet";
 const tweetList = [
   {
     userName: 'Donald Trump',
-    tweet: 'Windmills are the greatest threat in the US to both bald and golden eagles. Media claims fictional ‘global warming’ is worse.'
+    tweetBody: 'Windmills are the greatest threat in the US to both bald and golden eagles. Media claims fictional ‘global warming’ is worse.'
   },
   {
     userName: '6ix9nine',
-    tweet: 'in 2017 you guys thought you would have flying cars right? Instead you got ME ;)'
+    tweetBody: 'in 2017 you guys thought you would have flying cars right? Instead you got ME ;)'
   },
   {
     userName: 'Ryan Reynolds',
-    tweet: 'Id walk through fire for my daughter. Well not FIRE, because its dangerous. But a super humid room. But not too humid, because my hair'
+    tweetBody: 'Id walk through fire for my daughter. Well not FIRE, because its dangerous. But a super humid room. But not too humid, because my hair'
   },
   {
     userName: 'KANYE WEST',
-    tweet: 'IM SO HYPE RIGHT NOW  EVERYTHING HAS CHANGED... HAVE TALL EVER SEEN TRON? THE END OF TRON WHERE EVERYTHING LIGHT UP!!!!'
+    tweetBody: 'IM SO HYPE RIGHT NOW  EVERYTHING HAS CHANGED... HAVE TALL EVER SEEN TRON? THE END OF TRON WHERE EVERYTHING LIGHT UP!!!!'
   },
   {
     userName: 'Sweatpants Cher',
-    tweet: 'after 10 months out of school, the very first day of in-person learning, what do they do? after everything ive been through. they give my son a recorder to take home.'
+    tweetBody: 'after 10 months out of school, the very first day of in-person learning, what do they do? after everything ive been through. they give my son a recorder to take home.'
   }
 ];
 
@@ -29,7 +29,9 @@ function Feed() {
     <React.Fragment>
       <hr/>
       {tweetList.map((tweet, index) =>
-        <Tweet userName=
+        <Tweet userName={tweet.userName}
+          tweet={tweet.tweetBody}
+          key={index} />
       )}
     </React.Fragment>
   );
