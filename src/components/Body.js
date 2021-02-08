c6606c8cf72b5bc42bfb11a22ebf2bf62019ad42
Shannon Grantski <grantski@pm.me>
2021-02-08 14:36:06 -0800

    import React to all component files
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,3 +1,7 @@
-
+import React from "react";
+import Feed from "./Feed";
+import Suggestions from "./Suggestions";
+import Profile from "./Profile";
+import About from "./About";
 
 export default Body;
