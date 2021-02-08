5f077f0b4195579332b7b65a895eb7fb4e4cf6d5
Shannon Grantski <grantski@pm.me>
2021-02-08 14:33:17 -0800

    add initial imports to App.js
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Body from "./Body";
 
 function App(){
   return (
-    <h1>hello</h1>
+    <React.Fragment>
+      <h1>hello</h1>
+      <Header />
+      <Body />
+    </React.Fragment>
   );
 }
 
