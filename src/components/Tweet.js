044ef391f71d7e2532cc5718e916777c90a1878d
Shannon Grantski <grantski@pm.me>
2021-02-08 15:01:38 -0800

    build structure for twts
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,3 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function Tweet(props){
+  return(
+    <React.Fragment>
+      <h3>{props.userName}</h3>
+      <p><em>{props.tweetBody}</em></p>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+Tweet.propTypes = {
+  userName: PropTypes.string.isRequired,
+  tweet: PropTypes.string.isRequired
+};
 
 export default Tweet;
