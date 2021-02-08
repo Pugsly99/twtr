b1cc49ae00da8b5b8ec2e186b3425693907ad3f2
Shannon Grantski <grantski@pm.me>
2021-02-08 15:02:43 -0800

    add celebrity tweets to Feed
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,3 +1,38 @@
 import React from "react";
+import Tweet from "./Tweet";
+
+const tweetList = [
+  {
+    userName: 'Donald Trump',
+    tweet: 'Windmills are the greatest threat in the US to both bald and golden eagles. Media claims fictional ‘global warming’ is worse.'
+  },
+  {
+    userName: '6ix9nine',
+    tweet: 'in 2017 you guys thought you would have flying cars right? Instead you got ME ;)'
+  },
+  {
+    userName: 'Ryan Reynolds',
+    tweet: 'Id walk through fire for my daughter. Well not FIRE, because its dangerous. But a super humid room. But not too humid, because my hair'
+  },
+  {
+    userName: 'KANYE WEST',
+    tweet: 'IM SO HYPE RIGHT NOW  EVERYTHING HAS CHANGED... HAVE TALL EVER SEEN TRON? THE END OF TRON WHERE EVERYTHING LIGHT UP!!!!'
+  },
+  {
+    userName: 'Sweatpants Cher',
+    tweet: 'after 10 months out of school, the very first day of in-person learning, what do they do? after everything ive been through. they give my son a recorder to take home.'
+  }
+];
+
+function Feed() {
+  return (
+    <React.Fragment>
+      <hr/>
+      {tweetList.map((tweet, index) =>
+        <Tweet userName=
+      )}
+    </React.Fragment>
+  );
+}
 
 export default Feed;
